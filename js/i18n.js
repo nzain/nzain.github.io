a316ec4d80813baa345f4f7a1bdcbc7731489ede
Patrick Stalph <patrick.stalph@googlemail.com>
2026-05-25 09:11:07 +0200

    ui layout

--- js/i18n.js
+++ js/i18n.js
@@ -106,13 +106,13 @@
 
   var CLASS_IDS = [
     "bard",
-    "cleric",
     "druid",
+    "cleric",
     "paladin",
     "ranger",
+    "wiz",
     "sorc",
     "warlock",
-    "wiz",
   ];
 
   var LEVEL_IDS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
